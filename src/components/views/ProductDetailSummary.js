9c8f8c5d5d604e1370ebb43d5f6f5d384140e915
sumanchalki <suman.abc@gmail.com>
2019-03-19 19:00:23 +0530

    JSX expression added for product properties.

--- src/components/views/ProductDetailSummary.js
+++ src/components/views/ProductDetailSummary.js
@@ -3,19 +3,18 @@ import AddToCart from './AddToCart';
 import { Link } from 'react-router-dom';
 
 export default (props) => {
-  console.log(props);
   return(
-    <div class="col-6 col-sm-4">
+    <div className="col-6 col-sm-4" key={props.key}>
 
-      <div class="product-box">
-        <h3 class="prod-title"><Link to="/product-detail/{props.product.Id}">{props.product.Title}</Link></h3>
-        <Link to="/product-detail/{props.product.Id}"><img class="rounded mx-auto d-block" src={props.product.ImageUrl} /></Link>
-        <div class="prod-details">
-          <p class="description"> {props.product.Description}</p>
-          <p class="product-prop"><b>Category:</b> {props.product.Category}</p>
-          <p class="product-prop"><b>Made by:</b> {props.product.Manufacturer}</p>
-          <p class="product-prop"><b>Organic:</b> {props.product.Organic}</p>
-          <p class="product-prop"><b>Price:</b> {props.product.Price}</p>
+      <div className="product-box">
+        <h3 className="prod-title"><Link to={"/product-detail/" + props.product.Id}>{props.product.Title}</Link></h3>
+        <Link to={"/product-detail/" + props.product.Id}><img className="rounded mx-auto d-block" src={props.product.ImageUrl} /></Link>
+        <div className="prod-details">
+          <p className="description"> {props.product.Description}</p>
+          <p className="product-prop"><b>Category:</b> {props.product.Category}</p>
+          <p className="product-prop"><b>Made by:</b> {props.product.Manufacturer}</p>
+          <p className="product-prop"><b>Organic:</b> {props.product.Organic ? 'Yes' : 'No' }</p>
+          <p className="product-prop"><b>Price:</b> {props.product.Price}</p>
         </div>
       <AddToCart />
     </div>
