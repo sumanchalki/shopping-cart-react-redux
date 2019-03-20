62707ababa9c767e71709f6d25caa0bae201999c
sumanchalki <suman.abc@gmail.com>
2019-03-20 19:25:14 +0530

    Worked on page numbers display.

--- src/components/helpers/Pagination.js
+++ src/components/helpers/Pagination.js
@@ -7,15 +7,35 @@ export default class Pagination extends Component {
     //currentPage, perPage, totalProductCount
   }
 
+  generatePageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.props.totalProductCount / this.props.perPage); i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  }
+
+  generatePageMarkUp = () => {
+    const pageMarkUp1 = <li className="page-item"><a className="page-link" href="#">Previous</a></li>;
+    const pageMarkUp2 = this.generatePageNumbers().map(number => {
+      return <li className="page-item"><a className="page-link" href="#">{number}</a></li>;
+    });
+    const pageMarkUp3 = <li className="page-item"><a className="page-link" href="#">Next</a></li>;
+
+    return (
+      <React.Fragment>
+        {pageMarkUp1}
+        {pageMarkUp2}
+        {pageMarkUp3}
+      </React.Fragment>
+    );
+  }
+
   render() {
     if (this.props.totalProductCount > this.props.perPage) {
       return(
         <ul className="pagination product-pagination">
-          <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-          <li className="page-item"><a className="page-link" href="#">1</a></li>
-          <li className="page-item active"><a className="page-link" href="javascript:void(0);">2</a></li>
-          <li className="page-item"><a className="page-link" href="#">3</a></li>
-          <li className="page-item"><a className="page-link" href="#">Next</a></li>
+          {this.generatePageMarkUp()}
         </ul>
       );
     }
